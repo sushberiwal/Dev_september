208238489aedb8375aa23fc4269c8dd118c70dcc
sushberiwal <sushantberiwal@gmail.com>
2020-09-11 19:46:36 +0530

    got complete link

---
+++ Lec2_webScrapping/activity/homePage.js
@@ -0,0 +1,37 @@
+// npm install request
+// sudo npm install request
+
+let request = require("request");
+let fs = require("fs");
+let cheerio = require("cheerio");
+
+let link = "https://www.espncricinfo.com/series/_/id/8039/season/2019/icc-cricket-world-cup";
+
+
+request(link , dataReceiver );
+
+// succesfully data aa jaega => error => null , response.stausCode==200 and html = data
+// link is not valid => error  , response.statusCode == 404 and html = null
+
+//callback function
+function dataReceiver(error , response , html){
+    if(error == null && response.statusCode==200){
+        // successfully html received
+        // console.log(html);
+        parseData(html);
+    }
+    else if(response.statusCode == 404){
+        console.log("Page not found !!!")
+    }
+    else{
+        console.log(error);
+    }
+}
+
+function parseData(html){
+    let ch = cheerio.load(html);
+    let link = ch(".widget-items.cta-link a").attr("href");
+    let completeLink = "https://www.espncricinfo.com"+link;
+    console.log(completeLink);
+}
+
---
+++ reference.md
@@ -0,0 +1,8 @@
+# References
+
+## FreeCodeCamp
+* https://www.freecodecamp.org/learn/
+## Cheerio docs
+* https://cheerio.js.org/
+## FileSystem
+* https://nodejs.org/api/fs.html
