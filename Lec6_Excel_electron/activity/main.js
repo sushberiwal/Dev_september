a3a01d60c7ae5228c808cabbce12ba7d39ccf909
sushberiwal <sushantberiwal@gmail.com>
2020-09-20 20:17:38 +0530

    layout complete

--- Lec6_Excel_electron/activity/main.js
+++ Lec6_Excel_electron/activity/main.js
@@ -28,12 +28,7 @@ function createWindow(){
     win.webContents.openDevTools();
 }
 
-
 app.whenReady().then(createWindow);
-
-
-
-
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -43,7 +38,6 @@ app.on('window-all-closed', () => {
       app.quit()
     }
   })
-  
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -51,6 +45,5 @@ app.on('window-all-closed', () => {
       createWindow()
     }
   })
-  
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
