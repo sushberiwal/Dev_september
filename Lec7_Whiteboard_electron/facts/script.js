b2bb90f86519763e481ebb31c6d7893c019a4a49
sushberiwal <sushantberiwal@gmail.com>
2020-09-27 18:32:37 +0530

    dom manipulation

---
+++ Lec7_Whiteboard_electron/facts/script.js
@@ -0,0 +1,29 @@
+let body = document.querySelector("body");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+// let h1 = document.createElement("h1");
+// console.log(h1);
+// h1.innerText = "Hii i am added dynamically";
+// body.appendChild(h1);
+// let li = document.createElement("li");
+// li.innerText = "hii i am dynamic li";
+// ul.appendChild(li);
+// ul.innerHTML = `<li>Hii i am first li</li>
+//                  <li>Hii i am second li </li>
+//                  <li>Hii i am third li </li>`;
+
+
+//todo list
+btn.addEventListener("click" , function(){
+    let value = input.value;
+    input.value = "";
+    // console.log(value);
+    if(value){
+        let li = document.createElement("li");
+        li.innerText = value;
+        ul.appendChild(li);
+    }
+})
+
+
